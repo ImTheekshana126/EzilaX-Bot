454d2b76daa702db248cc0a21a139019c42d2ce8
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-04 06:17:39 -0700

    Update extraction.py

--- EzilaXBot/modules/helper_funcs/extraction.py
+++ EzilaXBot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from Sophia import LOGGER
-from Sophia.modules.users import get_user_id
+from EzilaXBot import LOGGER
+from EzilaXBot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
