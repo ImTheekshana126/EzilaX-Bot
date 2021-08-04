c4dceb55eaeccd4e2c620ae70ac1f1b99a266bf5
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-04 06:19:01 -0700

    Update handlers.py

--- EzilaXBot/modules/helper_funcs/handlers.py
+++ EzilaXBot/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import Sophia.modules.sql.blacklistusers_sql as sql
-from Sophia import ALLOW_EXCL
-from Sophia import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import EzilaXBot.modules.sql.blacklistusers_sql as sql
+from EzilaXBot import ALLOW_EXCL
+from EzilaXBot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
