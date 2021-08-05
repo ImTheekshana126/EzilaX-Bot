e0209f1eac58a34f780529d1936b745c3d56958d
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-04 20:26:10 -0700

    Update __init__.py

--- EzilaXBot/__init__.py
+++ EzilaXBot/__init__.py
@@ -204,7 +204,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from Sophia.modules.helper_funcs.handlers import (
+from EzilaXBot.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
